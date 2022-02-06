19a8d218d49aeea505bd8bbb2ec92294010876c8
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2022-02-05 20:55:34 -0500

    Adding files to repository
    Adding existing files
    - Main VR_Space.html file
    - Setup files
    - Assets

---
+++ public/js/object-behaviour.js
@@ -0,0 +1,70 @@
+AFRAME.registerComponent('generate-obj', {
+    init: function() {
+      let el = this.el;
+
+      this.addMarker = function(e){
+        let scene = document.querySelector('a-scene');
+
+        let newMark = document.createElement('a-sphere');
+        newMark.setAttribute('material','color: purple');
+        newMark.setAttribute('radius', '0.5');
+        newMark.setAttribute('position','1 1 -1');
+        // newMark.setAttribute('destroy-obj', {});
+        newMark.setAttribute('grabbable', {});
+        newMark.setAttribute('grabbing-obj', {});
+        newMark.setAttribute('droppable');
+        newMark.setAttribute('dynamic-body', 'mass:100');
+        newMark.setAttribute('id', 'boxy');
+        scene.appendChild(newMark);
+      }
+
+      this.el.addEventListener('click', this.addMarker);
+    },
+    remove: function(){
+        this.el.removeEventListener('click', this.addMarker)
+
+    }
+    
+});
+
+AFRAME.registerComponent('grabbing-obj', {
+    init: function() {
+      let el = this.el;
+      let thisObj = el.object3D.position;
+      let i =0;
+      
+
+      this.grab = function(e){
+        
+        if(thisObj.x < -0.5 && thisObj.x > -3 && thisObj.y < 1.5 && thisObj.y > -0.5 && thisObj.z < 0.25 && thisObj.z > -1.5){
+          console.log("it works");
+          el.setAttribute("color", 'red');
+          el.removeAttribute("dynamic-body");
+          el.setAttribute('animation', "property: position; to: -1 -10 -300; dur: 1500; easing: linear; loop: false");
+          console.log(thisObj.z);
+          
+        }
+        
+      }
+
+      this.el.addEventListener('dragend', this.grab);
+    },
+    remove: function(){
+      this.el.removeEventListener('dragend', this.grab);
+
+    },
+    tick: function() {
+      let el = this.el;
+      let thisObj = el.object3D.position;
+      //console.log(el);
+      
+      if(thisObj.z<-299){
+        console.log(thisObj.z);
+        el.parentElement.removeChild(el);
+      }
+    }
+
+    
+});
+
+
